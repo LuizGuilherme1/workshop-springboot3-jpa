f4d5d28d61928887b0dacaecc3d10dc02987b9fe
Luiz Guilherme <luizguilhermelocatelli@hotmail.com>
2023-12-03 08:36:53 -0300

    Category

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -18,7 +18,7 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable{
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
